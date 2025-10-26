7f9c3175245408732539c925a016dfd0856ae880
Kyugito <113055222+Kyugito666@users.noreply.github.com>
2025-10-26 20:16:45 +0700

    Update __init__.py

--- src/services/telegram/__init__.py
+++ src/services/telegram/__init__.py
@@ -1,1 +1,18 @@
+"""
+Telegram Package - Telegram API Integration
 
+Menyediakan:
+- Message sending dengan proxy support
+- Persona data formatting
+- Code block formatting
+"""
+
+from .sender import send_text_message, send_persona_to_telegram
+from .formatters import format_profile_message, format_code_message
+
+__all__ = [
+    'send_text_message',
+    'send_persona_to_telegram',
+    'format_profile_message',
+    'format_code_message'
+]
